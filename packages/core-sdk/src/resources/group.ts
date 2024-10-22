eaa9b70cc7a9a3ec01d24553e288a421a39cbf24
Bonnie <bonnie@57blocks.com>
2024-10-22 17:43:41 +0800

    Update pac

--- packages/core-sdk/src/resources/group.ts
+++ packages/core-sdk/src/resources/group.ts
@@ -67,7 +67,7 @@ export class GroupClient {
   /** Registers a Group IPA.
    * @param request - The request object containing necessary data to register group.
    *   @param request.groupPool The address specifying how royalty will be split amongst the pool of IPs in the group.
-   *   @param request.txOptions [Optional] This extends `WaitForTransactionReceiptParameters` from the Viem library, excluding the `hash` property.
+   *   @param request.txOptions [Optio1nal] This extends `WaitForTransactionReceiptParameters` from the Viem library, excluding the `hash` property.
    * @returns A Promise that resolves to a transaction hash, and if encodedTxDataOnly is true, includes encoded transaction data, and if waitForTransaction is true, includes group id.
    * @emits PGroupRegistered (groupId, groupPool);
    */
