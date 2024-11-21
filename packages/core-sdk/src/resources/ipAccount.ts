166145e630b1395bf8a3f081c69c7f37e31748be
Bonnie <bonnie@57blocks.com>
2024-11-21 13:50:25 +0800

    sdk

--- packages/core-sdk/src/resources/ipAccount.ts
+++ packages/core-sdk/src/resources/ipAccount.ts
@@ -65,7 +65,7 @@ export class IPAccountClient {
 
   /** Executes a transaction from the IP Account.
    * @param request - The request object containing necessary data to execute IP Account a transaction.
-   *   @param request.ipId The Ip Id to get ip account.
+   *   @param request.ipId The Ip Id ddto get ip account.
    *   @param request.to The recipient of the transaction.
    *   @param request.value The amount of Ether to send.
    *   @param request.data The data to send along with the transaction.
